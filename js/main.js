c210ded4bd6521ab84e7910adbc1a55185c36443
azukichan0220 <kamep1982@gmail.com>
2025-06-06 17:14:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -53,3 +53,15 @@ const swiper = new Swiper(".swiper_azuki", {
         disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
     },
 });
+
+// ***ここからハンバーガーメニュー***
+const drawerButton = document.getElementById("drawerButton");
+const drawerMenu = document.querySelector("header nav ul");
+
+function openDrawer() {
+    drawerMenu.classList.toggle("open");
+    drawerButton.classList.toggle("reDesign");
+}
+
+drawerButton.addEventListener("click", openDrawer);
+// ***ここまでハンバーガーメニュー***
