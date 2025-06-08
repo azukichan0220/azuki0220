a9b5c81a2af6b9ae90fd8b44b099bed1269b1071
azukichan0220 <kamep1982@gmail.com>
2025-06-08 23:53:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 const grid = document.getElementById("gridBackground");
 
 const images = [
-    "images/azukibk01.png",
-    "images/azukibk02.png",
-    "images/azukibk03.png",
-    "images/transparent.png",
-    "images/azukibk04.png",
-    "images/azukibk05.png",
-    "images/IMG_4666.png",
-    "images/transparent.png",
+  "images/azukibk01.png",
+  "images/azukibk02.png",
+  "images/azukibk03.png",
+  "images/transparent.png",
+  "images/azukibk04.png",
+  "images/azukibk05.png",
+  "images/IMG_4666.png",
+  "images/transparent.png",
 ];
 
 // ランダム表示。更新するたびに変わります。グリッド配置に無理やり透明のPNGをいれてランダムっぽくしてます。
@@ -22,36 +22,45 @@ const rows = Math.ceil(window.innerHeight / 100);
 const totalCells = cols * rows;
 
 for (let i = 0; i < totalCells; i++) {
-    const cell = document.createElement("div");
-    cell.className = "grid-item";
+  const cell = document.createElement("div");
+  cell.className = "grid-item";
 
-    const img = document.createElement("img");
-    const index = Math.floor(Math.random() * images.length);
-    img.src = images[index];
+  const img = document.createElement("img");
+  const index = Math.floor(Math.random() * images.length);
+  img.src = images[index];
 
-    // ランダム回転（見た目のバリエーション）
-    img.style.setProperty("--rotation", `${Math.floor(Math.random() * 360)}deg`);
+  // ランダム回転（見た目のバリエーション）
+  img.style.setProperty("--rotation", `${Math.floor(Math.random() * 360)}deg`);
 
-    // アニメーションのタイミングを少しずらす（自然な感じに）
-    img.style.animationDelay = `${Math.random() * 4}s`;
+  // アニメーションのタイミングを少しずらす（自然な感じに）
+  img.style.animationDelay = `${Math.random() * 4}s`;
 
-    cell.appendChild(img);
-    grid.appendChild(cell);
+  cell.appendChild(img);
+  grid.appendChild(cell);
 }
 
 //＜＜＜＜＜ ＜＜＜あしらいのため追加ここまで＞＞＞＞＞＞＞＞＞＞
 
 // スライダー
 const swiper = new Swiper(".swiper_azuki", {
-    loop: true, // ループ
-    slidesPerView: 3, // 一度に表示する枚数
-    speed: 6000, // ループの時間
-    allowTouchMove: false, // スワイプ無効
-    autoplay: {
-        //自動再生
-        delay: 0, // 途切れなくループ
-        disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
-    },
+  loop: true, // ループ
+  speed: 1500, // 少しゆっくり(デフォルトは300)
+  effect: "fade", // フェード
+  autoplay: {
+    // 自動再生
+    delay: 1000, // 2秒後に次のスライド
+    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  },
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 // ***ここからハンバーガーメニュー***
@@ -59,9 +68,19 @@ const drawerButton = document.getElementById("drawerButton");
 const drawerMenu = document.querySelector("header nav ul");
 
 function openDrawer() {
-    drawerMenu.classList.toggle("open");
-    drawerButton.classList.toggle("reDesign");
+  drawerMenu.classList.toggle("open");
+  drawerButton.classList.toggle("reDesign");
 }
 
 drawerButton.addEventListener("click", openDrawer);
 // ***ここまでハンバーガーメニュー***
+
+////////// loadingの部分　//////////
+// ローディング画面の要素を取得
+const loadingScreen = document.querySelector(".loading-screen");
+
+// ページの読み込みが完了したら実行される関数
+window.addEventListener("load", function () {
+  // ローディング画面を非表示にする
+  loadingScreen.classList.add("active");
+});
